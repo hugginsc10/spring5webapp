e99d8b272d94f41266a47a5c357019eb832833a5
Chas Huggins <hugginsc10@gmail.com>
2022-11-01 23:41:01 -0400

    added author and book joins tables

--- spring5webapp/src/main/java/guru/springgramework/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/guru/springgramework/spring5webapp/domain/Author.java
@@ -1,18 +1,38 @@
 package guru.springgramework.spring5webapp.domain;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
     private String firstName;
     private String lastName;
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
-
-
+    
+    
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
     }
-
+    
+    
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return this.firstName;
--- spring5webapp/src/main/java/guru/springgramework/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/guru/springgramework/spring5webapp/domain/Book.java
@@ -1,19 +1,40 @@
 package guru.springgramework.spring5webapp.domain;
-
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(Long id, String title, String isbn, Set<Author> authors) {
+        this.id = id;
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
     }
 
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return this.title;
